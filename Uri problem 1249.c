e5c2030a9ff5f938d20b62a5a63a9cb2f99a7207
pranto84 <sahapranto32@gmail.com>
2016-03-11 22:02:06 +0600

    Added files via upload

---
+++ Uri problem 1249.c
@@ -0,0 +1,29 @@
+//uri 1249
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int len,n;
+    char str[1001];
+    while(gets(str)!=NULL){
+        len=strlen(str);
+        int i=0,n=0;
+        while(i<len){
+            if(str[i] >= 'a' && str[i] <='z'){
+                if(str[i] + 13 > 'z')
+                    printf("%c",str[i] - 13);
+                else
+                    printf("%c",str[i] + 13);
+                  }
+            else if(str[i] >= 'A' && str[i] <='Z'){
+                if(str[i] + 13 > 'Z')
+                    printf("%c",str[i] - 13);
+                else
+                    printf("%c",str[i] + 13);
+                  }
+            else printf("%c",str[i]);
+            i++;
+        }
+        printf("\n");
+    }
+}
