38d1c2671d913a09dcca7fbb7b72338caa1734d1
pranto84 <sahapranto32@gmail.com>
2016-03-31 14:15:28 +0600

    গুণ

--- Uri problem 1004.c
+++ Uri problem 1004.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int A,B,PROD;
-    while(scanf("%d%d",&A,&B)==2){
-        PROD=A*B;
+    int A, B, PROD;
+    while(scanf("%d %d", &A, &B) == 2){
+        PROD = A * B;
         printf("PROD = %d\n", PROD);
     }
     return 0;
