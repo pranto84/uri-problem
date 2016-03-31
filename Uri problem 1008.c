4657741ba77495a88bba08fad48b2d6205246613
pranto84 <sahapranto32@gmail.com>
2016-03-31 14:23:57 +0600

    1, Salary

--- Uri problem 1008.c
+++ Uri problem 1008.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,t;
+    int n, t;
     double s;
-    while(scanf("%d%d%lf",&n,&t,&s)==3){
+    while( scanf(" %d %d %lf", &n, &t, &s) == 3){
         printf("NUMBER = %d\nSALARY = U$ %.2lf\n", n, t*s);
     }
     return 0;
