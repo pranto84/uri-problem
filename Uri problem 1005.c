03b5bed4bdcb4ff6c1b6012893dd15317a90fe67
pranto84 <sahapranto32@gmail.com>
2016-03-31 14:17:23 +0600

    easy, Average

--- Uri problem 1005.c
+++ Uri problem 1005.c
@@ -2,10 +2,10 @@
 int main()
 {
     float A,B;
-    while(scanf("%f%f",&A,&B)==2){
+    while( scanf("%f %f", &A, &B) == 2){
         float a;
-        a=A*3.5+B*7.5;
-        a=a/11;
+        a= A * 3.5 + B * 7.5;
+        a= a / 11;
         printf("MEDIA = %.5f\n", a);
     }
     return 0;
