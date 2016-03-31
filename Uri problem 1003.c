c0fa746d9391d34c841b05449d71ca08edb91006
pranto84 <sahapranto32@gmail.com>
2016-03-31 14:13:47 +0600

    Sum

--- Uri problem 1003.c
+++ Uri problem 1003.c
@@ -2,8 +2,8 @@
 int main()
 {
     int A, B, SOMA;
-    while(scanf("%d%d",&A,&B)==2){
-        SOMA = A+B;
+    while( scanf("%d%d", &A, &B) == 2){
+        SOMA = A + B;
         printf("SOMA = %d\n", SOMA);
     }
     return 0;
