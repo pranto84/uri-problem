d4297bd11cbfb8421d7fa041742093becc7875d7
pranto84 <sahapranto32@gmail.com>
2016-04-01 21:27:12 +0600

    string, after 13

--- Uri problem 1249.c
+++ Uri problem 1249.c
@@ -1,27 +1,26 @@
-//uri 1249
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-    int len,n;
+    int len;
     char str[1001];
-    while(gets(str)!=NULL){
-        len=strlen(str);
-        int i=0,n=0;
+    while( gets(str) != NULL ){
+        len = strlen(str);
+        int i = 0;
         while(i<len){
-            if(str[i] >= 'a' && str[i] <='z'){
+            if(str[i] >= 'a' && str[i] <= 'z'){
                 if(str[i] + 13 > 'z')
-                    printf("%c",str[i] - 13);
+                    printf("%c", str[i] - 13);
                 else
-                    printf("%c",str[i] + 13);
+                    printf("%c", str[i] + 13);
                   }
-            else if(str[i] >= 'A' && str[i] <='Z'){
+            else if(str[i] >= 'A' && str[i] <= 'Z'){
                 if(str[i] + 13 > 'Z')
-                    printf("%c",str[i] - 13);
+                    printf("%c", str[i] - 13);
                 else
-                    printf("%c",str[i] + 13);
+                    printf("%c", str[i] + 13);
                   }
-            else printf("%c",str[i]);
+            else printf("%c", str[i]);
             i++;
         }
         printf("\n");
