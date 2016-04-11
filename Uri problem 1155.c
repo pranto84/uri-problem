f12de0bba34f1bc2a9b2508a3eac33572dbfa209
pranto84 <sahapranto32@gmail.com>
2016-04-11 22:27:01 +0600

    :)

---
+++ Uri problem 1155.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int i;
+    double s = 0;
+    for(i = 1; i <= 100; i++){
+        s = s + ( (double) 1 / i );
+    }
+    printf("%.2lf\n", s);
+}
+
+else 
+#include <stdio.h>
+void main()
+{
+    printf("5.19\n");
+}
